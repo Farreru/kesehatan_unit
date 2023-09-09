3a4b96005565fad74dd39142c96b3163839ddd39
unknown <m.adit.farrel@gmail.com>
2023-09-10 02:56:46 +0700

    Second Commit

---
+++ halaman/login/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="../../assets/gambar/logo.png" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Halaman Login</title>
+</head>
+
+<body>
+
+    <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+        <div class="card" style="width: 25rem;">
+            <div class="card-body border-top border-primary">
+                <div class="d-grid mb-3">
+                    <img src="../../assets/gambar/logo.png" class="img-fluid mx-auto " width="80px" alt=""><span class="text-center text-muted"><b>Kesehatan Unit</b></span>
+                </div>
+                <h5 class="card-title mb-3">Login</h5>
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="">Username</label>
+                        <input type="text" name="" class="form-control mb-3" id="" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Password</label>
+                        <input type="text" name="" class="form-control mb-3" id="" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" name="login" value="Login">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="../../assets/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
